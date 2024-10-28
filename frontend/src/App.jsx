6d2a663717baa6e7929ef76302bede929d8154e9
ASLawan <nawalnitsua@gmail.com>
2024-10-28 04:14:15 +0100

    Update - app main files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,77 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { loginUser } from "./redux/authSlice";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/HomePage";
+// import Shop from "./pages/ProductList";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/login";
+import AdminRoute from "./components/AdminRoute";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminProducts from "./pages/admin/AdminProducts";
+import AdminOrders from "./pages/admin/AdminOrders";
+import AdminUsers from "./pages/admin/AdminUsers";
+import AdminAddProduct from "./pages/admin/AdminAddProduct";
+import AdminAddUser from "./pages/admin/AdminAddUser";
+import "./App.css";
+import Cart from "./pages/Cart";
+import ProductDetails from "./pages/ProductDetails";
 
-function App() {
+const App = () => {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+
+  useEffect(() => {
+    if (token) {
+      // Optionally, you can make an API call here to validate the token
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      // Load the user profile if the token is valid
+      dispatch(loginUser(token));
+    }
+  }, [dispatch, token]);
   return (
     <>
-      <div>
-        <h2>Luxury Beads</h2>
-      </div>
+      <Router>
+        <Navbar user={user} />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/admin"
+            element={
+              <AdminRoute>
+                <AdminDashboard />
+              </AdminRoute>
+            }
+          >
+            <Route path="products" element={<AdminProducts />} />
+            <Route path="addproduct" element={<AdminAddProduct />} />
+            <Route path="orders" element={<AdminOrders />} />
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="adduser" element={<AdminAddUser />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+      </Router>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
